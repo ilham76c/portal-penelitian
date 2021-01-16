c7160de99d4c9cc6f12bddbb84c1ce230372b168
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-01-17 00:05:20 +0700

    Feature: menambahkan feature login dan logout menggunakan Jetstream livewire --teams

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -111,9 +111,20 @@
                         </ul>
                     </li>
                     <li class="header">LABELS</li>
-                    <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-                    <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-                    <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+                    <li>
+                        <form action="{{ url('logout') }}" method="post">
+                            @method('POST')
+                            @csrf
+                            <button class="btn btn-link" type="submit">
+                                <i class="fa fa-circle-o text-red text-right"></i> 
+                                &nbsp;
+                                <span>Logout</span>
+                            </button>
+                        </form>                    
+                    </li>
+                    
+                    <!-- <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
+                    <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> -->
                 </ul>
             </section>
             <!-- /.sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,23 @@ Route::get("/", [UserController::class, "home"]);
 Route::get("/penelitian", [UserController::class, "penelitian"]);
 Route::get("/aplikasi", [UserController::class, "aplikasi"]);
 
-Route::get("/penelitian/table", [PenelitianController::class, "index"]);
-Route::post("/penelitian/store", [PenelitianController::class, "store"]);
-Route::get("/penelitian/form/tambah", [PenelitianController::class, "create"]);
-Route::put("/penelitian/update/{penelitianModel}", [PenelitianController::class, "update"]);
-Route::get("/penelitian/form/{penelitianModel}/edit", [PenelitianController::class, "edit"]);
-Route::get("/penelitian/show/{penelitianModel}", [PenelitianController::class, "show"]);
-Route::delete("/penelitian/delete/{penelitianModel}", [PenelitianController::class, "destroy"]);
 Route::get("/penelitian/download/{fileName}", [PenelitianController::class, "download"]);
 
-Route::get("/aplikasi/table", [AplikasiController::class, "index"]);
-Route::get("/aplikasi/form/tambah", [AplikasiController::class,"create"]);
-Route::post("/aplikasi/store", [AplikasiController::class,"store"]);
-Route::get("/aplikasi/form/{aplikasiModel}/edit", [AplikasiController::class, "edit"]);
-Route::put("/aplikasi/update/{aplikasiModel}", [AplikasiController::class, "update"]);
-Route::delete("/aplikasi/delete/{aplikasiModel}", [AplikasiController::class, "destroy"]);
+Route::middleware(['auth:sanctum', 'verified'])->group(function() {
+    Route::get("/dashboard", [AdminController::class, "index"]);
 
-Route::get("/dashboard", [AdminController::class, "index"]);
-Route::get("/aplikasi/form", [AdminController::class, "aplikasiForm"]);
+    Route::get("/penelitian/table", [PenelitianController::class, "index"]);
+    Route::post("/penelitian/store", [PenelitianController::class, "store"]);
+    Route::get("/penelitian/form/tambah", [PenelitianController::class, "create"]);
+    Route::put("/penelitian/update/{penelitianModel}", [PenelitianController::class, "update"]);
+    Route::get("/penelitian/form/{penelitianModel}/edit", [PenelitianController::class, "edit"]);
+    Route::get("/penelitian/show/{penelitianModel}", [PenelitianController::class, "show"]);
+    Route::delete("/penelitian/delete/{penelitianModel}", [PenelitianController::class, "destroy"]);
 
+    Route::get("/aplikasi/form/tambah", [AplikasiController::class,"create"]);
+    Route::get("/aplikasi/table", [AplikasiController::class, "index"]);
+    Route::post("/aplikasi/store", [AplikasiController::class,"store"]);
+    Route::get("/aplikasi/form/{aplikasiModel}/edit", [AplikasiController::class, "edit"]);
+    Route::put("/aplikasi/update/{aplikasiModel}", [AplikasiController::class, "update"]);
+    Route::delete("/aplikasi/delete/{aplikasiModel}", [AplikasiController::class, "destroy"]);
+});
