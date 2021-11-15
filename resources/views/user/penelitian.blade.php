53cfd1f01c049a59ee980dfe6c27e2f4ff79dd66
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-11-15 23:52:36 +0700

    menambahkan function untuk mencari data yg cocok dengan text pencarian

--- resources/views/user/penelitian.blade.php
+++ resources/views/user/penelitian.blade.php
@@ -3,16 +3,16 @@
 @section('title', 'Web Ta | Skripsi')
 
 @section('main')
-<main class="yrav-bg-abu">
+<main class="yrav-bg-abu" style="min-height: 80vh;">
     <!-- Underlined search bars with buttons -->
     <div class="container pt-4 pt-lg-5 pb-2 pb-lg-4 pb-md-3">
         <form>
             <div class="row justify-content-center">
                 <div class="form-group col-7">
-                    <input id="exampleFormControlInput5" type="text" placeholder="Pencarian" class="form-control form-control-underlined yrav-bg-abu">
+                    <input id="text-search" type="text" placeholder="Pencarian" class="form-control form-control-underlined yrav-bg-abu">
                 </div>
                 <div class="form-group col-1">
-                    <button type="submit" class="btn btn-primary rounded-pill shadow-sm">
+                    <button type="button" id="btn-search" class="btn btn-primary rounded-pill shadow-sm">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
                             <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
@@ -25,7 +25,7 @@
     <!-- End -->
     <!-- Content -->
     <!-- <div class="container-fluid p-5"> -->
-    <div class="container-fluid d-flex justify-content-center pb-5 flex-wrap"> 
+    <div id="penelitian" class="container-fluid d-flex justify-content-center pb-5 flex-wrap"> 
         @forelse ($penelitian as $p)
             <div class="card shadow mx-1 mx-lg-3 mx-md-2 my-2 my-lg-4 my-md-3" style="width: 22rem;">
                 <div class="card-body d-flex flex-column yrav-card-effect">
@@ -105,7 +105,7 @@
 @section('js')
 <script>
     $(document).ready(function() {
-        $(".btn-abstrak").on("click", function() {            
+        $(document).on("click", ".btn-abstrak", function() {            
             $.ajax({
                 //url: `/skripsi/show/${$(this).data("id")}`,
                 url: this.href,
@@ -121,6 +121,57 @@
                     console.log(e.responseText);
                 }
             });
+        });
+
+        $("#text-search").change(function() {
+            console.log(this.value)
+            const penelitian = {!! json_encode($penelitian) !!};
+            const search_result = [];
+            for (const iterator in penelitian.data) {
+                if (
+                    (jQuery(penelitian.data[iterator].judul).text()).includes($('#text-search').val().toLowerCase())
+                    ||
+                    penelitian.data[iterator].kata_kunci.includes($('#text-search').val().toLowerCase())
+                    ||
+                    (jQuery(penelitian.data[iterator].abstract).text()).includes($('#text-search').val().toLowerCase())
+                ) {
+                    search_result.push(penelitian.data[iterator])
+                }
+            }
+
+            let penelitian_card = '';
+            for (const key of search_result) {
+                penelitian_card += `
+                <div class="card shadow mx-1 mx-lg-3 mx-md-2 my-2 my-lg-4 my-md-3" style="width: 22rem;">
+                    <div class="card-body d-flex flex-column yrav-card-effect">
+                        <div class="d-flex">                                
+                            <div class="d-flex flex-column align-items-center">
+                                <svg width="40px" height="40px" viewBox="0 0 16 16" class="bi bi-journal-text" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+                                    <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+                                    <path fill-rule="evenodd" d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+                                </svg>
+                                <p class="text-muted mt-1">${key.tahun}</p>
+                            </div>                                                                    
+                            <h5 class="card-title ml-3">${key.judul}</h5>                                
+                        </div>
+                                                    
+                        <div class="card-text pb-4">     
+                            Penulis:
+                            <table class="table-responsive text-muted">
+                                <tr>
+                                    <td class="align-top">&emsp;-&nbsp;</td>
+                                    <td>${key.penulis} (${key.nrp})</td>
+                                </tr>
+                            </table>                                                            
+                        </div>
+                        <a href='{{ secure_url("/penelitian/show/") }}/${key.id}' class="card-link mt-auto btn btn-outline-info btn-sm btn-abstrak" data-toggle="modal">Detail</a>
+                    </div>
+                </div>
+                `;
+            }
+
+            $('#penelitian').html(penelitian_card);
         });
     });    
 </script>
