edc1d88227fc9ba88207a6f6ba64e90c9a7bb634
Moh. Ilham Burhanuddin <m.ilham.b.97@gmail.com>
2021-11-21 00:03:23 +0700

    add setPath() to paginate link

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,12 +16,14 @@ class UserController extends Controller
     public function penelitian()
     {        
         $penelitian = PenelitianModel::paginate(6);
+        $penelitian->setPath('/penelitian');
         return view("user.penelitian", compact("penelitian"));
     }
 
     public function aplikasi()
     {
         $aplikasi = AplikasiModel::paginate(6);
+        $aplikasi->setPath('/aplikasi');
         return view("user.aplikasi", compact("aplikasi"));
     }
 }
